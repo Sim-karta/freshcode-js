d9faa7ab6c8bccc7fec15bcb63ff589268579c00
Sim-karta <simonenkonikita561@gmail.com>
2026-07-25 20:00:03 +0300

    Make registration for student

---
+++ assets/scripts/study.js
@@ -0,0 +1,17 @@
+const firstName = prompt("Введіть ім'я");
+const lastName = prompt("Введіть прізвище");
+const specialty = prompt("Введіть спеціальність");
+const course = prompt("Введіть курс");
+const averageGrade = prompt("Введіть середній бал");
+
+console.log(
+    "===== Профіль студента =====\nПІБ:",
+    firstName,
+    lastName,
+    "\nСпеціальність:",
+    specialty,
+    "\nКурс: ",
+    course,
+    " \nСередній бал:",
+    averageGrade,
+);
