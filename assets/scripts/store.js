36a0ccc8a0ec10b359007f228a4d8cb18595a441
Sim-karta <simonenkonikita561@gmail.com>
2026-07-25 20:55:44 +0300

    Make running code when the buttons is clicked

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,41 +1,47 @@
-const price = Number(prompt("Введіть ціну товару"));
-const num = Number(prompt("Введіть кількість товару"));
+const storeBtn = document.querySelector("#store-btn");
 
-const paid = price * num;
-let discount = 0;
+const store = () => {
+    const price = Number(prompt("Введіть ціну товару"));
+    const num = Number(prompt("Введіть кількість товару"));
 
-if (paid >= 800) {
-    discount = (paid / 100) * 5;
-} else if (paid >= 500) {
-    discount = (paid / 100) * 3;
-}
+    const paid = price * num;
+    let discount = 0;
 
-const deliveryCh = Number(
-    prompt(
-        "Виберість спосіб доставки:\n- 1 — Самовивіз (0 грн);\n- 2 — Нова пошта (100 грн);\n- 3 — Кур'єр (150 грн).\nПри замовлені від 1000 грн доставка безкоштовна.",
-    ),
-);
-let deliveryPrice;
-
-if (paid - discount >= 1000) {
-    deliveryPrice = 0;
-} else {
-    switch (deliveryCh) {
-        case 1:
-            deliveryPrice = 0;
-            break;
-        case 2:
-            deliveryPrice = 100;
-            break;
-        case 3:
-            deliveryPrice = 150;
-            break;
+    if (paid >= 800) {
+        discount = (paid / 100) * 5;
+    } else if (paid >= 500) {
+        discount = (paid / 100) * 3;
     }
-}
 
-const finalPrice = paid - discount + deliveryPrice;
+    const deliveryCh = Number(
+        prompt(
+            "Виберість спосіб доставки:\n- 1 — Самовивіз (0 грн);\n- 2 — Нова пошта (100 грн);\n- 3 — Кур'єр (150 грн).\nПри замовлені від 1000 грн доставка безкоштовна.",
+        ),
+    );
+    let deliveryPrice;
 
-console.log("Ціна без знижки - ", paid);
-console.log("Знижка - ", discount);
-console.log("Доставка - ", deliveryPrice);
-console.log("До сплати - ", finalPrice);
+    if (paid - discount >= 1000) {
+        deliveryPrice = 0;
+    } else {
+        switch (deliveryCh) {
+            case 1:
+                deliveryPrice = 0;
+                break;
+            case 2:
+                deliveryPrice = 100;
+                break;
+            case 3:
+                deliveryPrice = 150;
+                break;
+        }
+    }
+
+    const finalPrice = paid - discount + deliveryPrice;
+
+    console.log("Ціна без знижки - ", paid);
+    console.log("Знижка - ", discount);
+    console.log("Доставка - ", deliveryPrice);
+    console.log("До сплати - ", finalPrice);
+};
+
+storeBtn.addEventListener("click", store);
