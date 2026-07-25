30ee643cd88f67651b9814296614cf449cf5e295
Sim-karta <simonenkonikita561@gmail.com>
2026-07-25 18:34:09 +0300

    Add discount

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -2,5 +2,16 @@ const price = Number(prompt("Введіть ціну товару"));
 const num = Number(prompt("Введіть кількість товару"));
 
 const paid = price * num;
+let discount = 0;
 
-console.log(paid);
+if (paid >= 800) {
+    discount = (paid / 100) * 5;
+} else if (paid >= 500) {
+    discount = (paid / 100) * 3;
+}
+
+const finalPrice = paid - discount;
+
+console.log("Ціна без знижки - ", paid);
+console.log("Знижка - ", discount);
+console.log("До сплати - ", finalPrice);
