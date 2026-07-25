25759aa504a1f648b12a21a13f0620f287d30c62
Sim-karta <simonenkonikita561@gmail.com>
2026-07-25 20:11:54 +0300

    Add access check

--- assets/scripts/study.js
+++ assets/scripts/study.js
@@ -4,6 +4,10 @@ const specialty = prompt("Введіть спеціальність");
 const course = prompt("Введіть курс");
 const averageGrade = prompt("Введіть середній бал");
 
+const isStudent = confirm("isStudent\n(ОК - true/ Відміна - false)");
+const hasPaid = confirm("hasPaid\n(ОК - true/ Відміна - false)");
+const isMentor = confirm("isMentor\n(ОК - true/ Відміна - false)");
+
 console.log(
     "===== Профіль студента =====\nПІБ:",
     firstName,
@@ -15,3 +19,9 @@ console.log(
     " \nСередній бал:",
     averageGrade,
 );
+
+if ((isStudent && hasPaid) || isMentor) {
+    console.log("Ви маєте доступ до курсу");
+} else {
+    console.log("Ви не маєте доступ до курсу");
+}
