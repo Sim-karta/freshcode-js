28c1708ed539c308d58ffc6b3b96592d91785c7f
Sim-karta <simonenkonikita561@gmail.com>
2026-07-25 18:53:44 +0300

    Add delivery system

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -10,8 +10,32 @@ if (paid >= 800) {
     discount = (paid / 100) * 3;
 }
 
-const finalPrice = paid - discount;
+const deliveryCh = Number(
+    prompt(
+        "Виберість спосіб доставки:\n- 1 — Самовивіз (0 грн);\n- 2 — Нова пошта (100 грн);\n- 3 — Кур'єр (150 грн).\nПри замовлені від 1000 грн доставка безкоштовна.",
+    ),
+);
+let deliveryPrice;
+
+if (paid - discount >= 1000) {
+    deliveryPrice = 0;
+} else {
+    switch (deliveryCh) {
+        case 1:
+            deliveryPrice = 0;
+            break;
+        case 2:
+            deliveryPrice = 100;
+            break;
+        case 3:
+            deliveryPrice = 150;
+            break;
+    }
+}
+
+const finalPrice = paid - discount + deliveryPrice;
 
 console.log("Ціна без знижки - ", paid);
 console.log("Знижка - ", discount);
+console.log("Доставка - ", deliveryPrice);
 console.log("До сплати - ", finalPrice);
